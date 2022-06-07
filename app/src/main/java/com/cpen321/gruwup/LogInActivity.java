9803bbc41c8b3c53cabe41581156cdb7a854e2f3
Sijan <sijuichi1@gmail.com>
2022-06-07 01:32:00 -0700

    Implemented Login Functionality

---
+++ app/src/main/java/com/cpen321/gruwup/LogInActivity.java
@@ -0,0 +1,126 @@
+package com.cpen321.gruwup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class LogInActivity extends AppCompatActivity {
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 1;
+    final static String TAG = "LogInActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        getSupportActionBar().hide();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        // TO DO : Add Use without SignIn Option to be able to go to MainActivity without
+        // sign in
+
+
+
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null){
+            Log.d(TAG, "There is no user signed in");
+        }
+        else{
+            // TO DO:  navigate to MainActivity with User Profile Information Passed In
+
+            Log.d(TAG, "Display Name: " + account.getDisplayName());
+            Log.d(TAG, "Email: " + account.getEmail());
+            Log.d(TAG, "Given Name: "+ account.getGivenName());
+            Log.d(TAG, "Family Name: "+ account.getFamilyName());
+            Log.d(TAG, "Photo URL: "+ account.getPhotoUrl());
+
+            Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+//            myIntent.putExtra("key", value); //Optional parameters
+            Bundle extras = new Bundle();
+
+            String imageUrl = "null";
+
+            if (account.getPhotoUrl() != null){
+                imageUrl = account.getPhotoUrl().toString();
+            }
+
+            extras.putString("Display_Name", account.getDisplayName() );
+            // Note: Photo URL is converted to String
+            extras.putString("Photo_URL", imageUrl);
+            intent.putExtras(extras);
+            startActivity(intent);
+
+        }
+
+    }
+}
