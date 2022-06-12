9c460bf7c23156cf36a37239ba3ab7f3b5c15d71
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-06-11 20:41:09 -0700

    8 added return data for account and request

--- backend/routes/Account.js
+++ backend/routes/Account.js
@@ -1,4 +1,53 @@
 const express = require("express");
+const DataValidator = require("./../Constants/DataValidator.js");
 const router = express.Router();
+
+// TODO: need database to store profile information based on user token
+
+// can delete this later, just for early testing
+const example = {
+    "userId": "string",
+    "name": "Bob John",
+    "biography": "I am a 20 year old living in Vancouver",
+    "categories": [1, 2, 3]
+  }
+
+router.post("/create", (req, res) => {
+    if (DataValidator.isTokenValid(req.body.userId)) {
+        // TODO: check fields are also valid
+        res.status(200).send(example);
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
+
+router.post("/sign-in", (req, res) => {
+    if (DataValidator.isTokenValid(req.body.userId)) {
+        res.status(200).send(example);
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
+
+router.post("/sign-out", (req, res) => {
+    if (DataValidator.isTokenValid(req.body.userId)) {
+        res.sendStatus(200);
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
+
+router.post("/edit", (req, res) => {
+    if (DataValidator.isTokenValid(req.body.userId)) {
+        // TODO: check fields are also valid
+        res.status(200).send(example);
+    }
+    else {
+        res.sendStatus(400);
+    }
+});
 
 module.exports = router;
