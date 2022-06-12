6b62d639e53d9f115561e2356d0b5dcac91d093c
JerryShao3234 <shaojerry@icloud.com>
2022-06-12 14:23:12 -0700

    implemented google sign out, needs minor adjustment

--- app/src/main/java/com/cpen321/gruwup/ProfileFragment.java
+++ app/src/main/java/com/cpen321/gruwup/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.cpen321.gruwup;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,16 +13,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
     TextView displayName;
     ImageView profilePic;
+    Button signOutButton;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_profile, container, false);
+
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), GoogleSignInOptions.DEFAULT_SIGN_IN);
 
         displayName = (TextView) view.findViewById(R.id.userName);
         displayName.setText(this.getArguments().getString("Display_Name"));
@@ -32,6 +41,17 @@ public class ProfileFragment extends Fragment {
         //set profile picture using the link from the bundle using Picasso
 
 
+        signOutButton = (Button) view.findViewById(R.id.sign_out_button);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("ProfileFragment", "Signing out");
+                mGoogleSignInClient.signOut();
+                //should return user to login screen in the LoginActivity
+            }
+        });
+
         return view;
     }
+
 }
