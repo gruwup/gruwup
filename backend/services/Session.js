98e277a6aea7d55691791dfc639614b4b0972a0d
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-06-22 23:16:39 -0700

    52 Added cookies and sessions

--- backend/services/Session.js
+++ backend/services/Session.js
@@ -1,5 +1,20 @@
+const CryptoJS = require("crypto-js");
 const session = {};
+const name = "gruwup-session";
 
-const createSession = () => {
-
+const createSession = (userId) => {
+    var encrypt = CryptoJS.SHA256(userId + Date.now());
+    var token = CryptoJS.enc.Hex.stringify(encrypt);
+    session[name + "=" + token] = userId;
+    return token;
 }
+
+const getSessionInfo = (token) => {
+    return session[token];
+}
+
+const deleteSession = (token) => {
+    delete session[token];
+}
+
+module.exports = {createSession, deleteSession, getSessionInfo, name};
