d9d076a735ba104daa3c5b5b6646bde7a0098dfb
Sijan <sijuichi1@gmail.com>
2022-06-22 19:54:55 -0700

    Added Chat Activity

---
+++ app/src/main/java/com/cpen321/gruwup/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.cpen321.gruwup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_chat);
+    }
+}
