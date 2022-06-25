e3f95e7e7384894dc4a1a1915ac06aedbea2f922
Grant Li <grant@jostle.me>
2022-06-24 22:12:25 -0700

    58 refractored cancel and update adventure

--- backend/routes/Adventure.js
+++ backend/routes/Adventure.js
@@ -76,21 +76,45 @@ router.get("/:adventureId/detail", async (req, res) => {
 });
 
 // update adventure details
-router.put("/:adventureId/update", (req, res) => {
+router.put("/:adventureId/update", async (req, res) => {
     // TODO: validate token
+    var adventure = {
+        owner: req.body.owner,
+        title: req.body.title,
+        description: req.body.description,
+        peopleGoing: [req.body.owner],
+        dateTime: req.body.dateTime,
+        location: req.body.location,
+        category: req.body.category,
+        status: "OPEN",
+        image: req.body.image ? new Buffer(req.body.image.split(",")[1],"base64") : null,
+        city: req.body.location.split(", ")[1] ?? "unknown"
+    };
     try {
-        AdventureStore.updateAdventure(req, res);
+        var result = await AdventureStore.updateAdventure(req.params.adventureId, adventure);
+        if (result.code === 200) {
+            res.status(200).send(result.payload);
+        }
+        else {
+            res.status(result.code).send(result.message);
+        }
     } catch (err) {
         res.status(500).send(err);
     }
 });
 
 // cancel the adventure and delete chat room
-router.put("/:adventureId/cancel", (req, res) => {
+router.put("/:adventureId/cancel", async (req, res) => {
     // TODO: validate token
     // TODO: delete chat room
     try {
-        AdventureStore.cancelAdventure(req, res);
+        var result = await AdventureStore.cancelAdventure(req, res);
+        if (result.code === 200) {
+            res.status(200).send(result.payload);
+        }
+        else {
+            res.status(result.code).send(result.message);
+        }
     } catch (err) {
         res.status(500).send(err);
     }
--- backend/store/AdventureStore.js
+++ backend/store/AdventureStore.js
@@ -46,50 +46,53 @@ module.exports = class AdventureStore {
         };
     };
 
-    static updateAdventure = async (req, res) => {
-        Adventure.findOneAndUpdate(
-            { _id: req.params.adventureId },
-            { $set: { title: req.body.title, description: req.body.description, peopleGoing:req.body.peopleGoing, dateTime: req.body.dateTime, location: req.body.location, category: req.body.category, image: req.body.image ? new Buffer(req.body.image.split(",")[1],"base64") : null } },
-            {new: true},
-            (err, adventure) => {
-                if (err) {
-                    res.status(500).send({
-                        message: err.toString()
-                    });
-                }
-                else if (!adventure) {
-                    res.status(404).send({
-                        message: "Adventure not found"
-                    });
-                }
-                else {
-                    res.status(200).send(adventure);
-                }
-            }
-        );
+    static updateAdventure = async (adventureId, adventure) => {
+        try {
+            var result = await Adventure.findOneAndUpdate(
+                { _id: adventureId },
+                { $set: adventure },
+                {new: true});
+            console.log("updated result: " + result);
+            return {
+                code: 200,
+                message: "Adventure updated successfully",
+                payload: result
+            };
+        }
+        catch (err) {
+            return {
+                code: 500,
+                message: err
+            };
+        }
     };
 
     static cancelAdventure = async (req, res) => {
-        Adventure.findOneAndUpdate(
-            { _id: req.params.adventureId },
-            { $set: { status: "CANCELLED" } },
-            {new: true},
-            (err, adventure) => {
-                if (err) {
-                    res.status(500).send({
-                        message: err.toString()
-                    });
-                }
-                else if (!adventure) {
-                    res.status(404).send({
-                        message: "Adventure not found"
-                    });
-                }
-                else {
-                    res.status(200).send(adventure);
-                }
+        try {
+            var result = await Adventure.findOneAndUpdate(
+                { _id: req.params.adventureId },
+                { $set: { status: "CANCELLED" } },
+                {new: true});
+            if (result) {
+                return {
+                    code: 200,
+                    message: "Adventure cancelled successfully",
+                    payload: result
+                };
             }
-        );
+            else {
+                return {
+                    code: 404,
+                    message: "Adventure not found"
+                };
+            }
+        }
+        catch (err) {
+            return {
+                code: 500,
+                message: err
+            };
+        }
     };
 
     static getUsersAdventures = async (userId) => {
