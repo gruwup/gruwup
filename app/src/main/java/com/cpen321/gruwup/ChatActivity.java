9408838f5d797b217fe4aeb7d31e30085c02cdcd
Sijan <sijuichi1@gmail.com>
2022-07-04 08:21:04 -0700

    Added message instances in group

--- app/src/main/java/com/cpen321/gruwup/ChatActivity.java
+++ app/src/main/java/com/cpen321/gruwup/ChatActivity.java
@@ -2,12 +2,21 @@ package com.cpen321.gruwup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView messageRecyclerView;
+    ArrayList<Message> messages = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +30,20 @@ public class ChatActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        messageRecyclerView = findViewById(R.id.messageRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        messageRecyclerView.setLayoutManager(linearLayoutManager);
+
+        initMessages();
+        MessageViewAdapter adapter = new MessageViewAdapter(this,messages);
+        messageRecyclerView.setAdapter(adapter);
+    }
+
+    private  void initMessages(){
+        Message m1 =  new Message("1", "sara", "hellooooo", "1111", "11111");
+        messages.add(m1);
+
     }
 }
