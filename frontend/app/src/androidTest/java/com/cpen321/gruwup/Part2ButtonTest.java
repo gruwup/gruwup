be56303cb8ec5de25c91e07fc5c50fdfab9a9869
JerryShao3234 <shaojerry@icloud.com>
2022-07-12 20:36:45 -0700

    Done Part 2 FE

--- frontend/app/src/androidTest/java/com/cpen321/gruwup/Part2ButtonTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/gruwup/Part2ButtonTest.java
@@ -1,8 +1,10 @@
 package com.cpen321.gruwup;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Context;
@@ -24,15 +26,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class Part2ButtonTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void navBarClickOnSearch() { //checks that Search Fragment can be opened from navbar
         assert(true);
         onView(withText("Choose and Create Adventures!")).check(matches(isDisplayed()));
+        onView((withId(R.id.nav_search))).check(matches(isDisplayed())).perform(click());
+        onView(withText("Search Results")).check(matches(isDisplayed()));
     }
 }
 
