5bb75dd75959dc937baf07c5b7b4d4c694ecf79e
JerryShao3234 <shaojerry@icloud.com>
2022-07-12 17:57:24 -0700

    Test Expresso

--- frontend/app/src/androidTest/java/com/cpen321/gruwup/ExampleInstrumentedTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/gruwup/ExampleInstrumentedTest.java
@@ -1,10 +1,18 @@
 package com.cpen321.gruwup;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +25,14 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.cpen321.gruwup", appContext.getPackageName());
+    public void listGoesOverTheFold() {
+        assert(true);
+        onView(withText("Choose and Create Adventures!")).check(matches(isDisplayed()));
     }
 }
+
