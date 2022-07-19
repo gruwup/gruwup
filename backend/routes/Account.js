7d1be9f7a582eb8cfa929e3e04b59138753713b0
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-07-18 23:27:46 -0700

    103 fix routes styling and return after sending reponse

--- backend/routes/Account.js
+++ backend/routes/Account.js
@@ -12,22 +12,20 @@ router.post("/sign-in", async (req, res) => {
     
     res.cookie(Session.name, token);
     if (result.code === 200) {
-        res.status(200).send({ userId, userExists: true });
+        return res.status(200).send({ userId, userExists: true });
     }
     else if (result.code === 404) {
-        res.status(404).send({ userId, userExists: false });
+        return res.status(404).send({ userId, userExists: false });
     }
-    res.sendStatus(400);
+    return res.sendStatus(400);
 });
 
 router.post("/sign-out", (req, res) => {
     if (Session.validSession(req.headers.cookie)) {
         Session.deleteSession(req.headers.cookie);
-        res.sendStatus(200);
+        return res.sendStatus(200);
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
+    return res.status(403).send({ message: Session.invalid_msg });
 });
 
 module.exports = router;
