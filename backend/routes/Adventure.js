e06777417f21a2a10edc6a81734b01e479adcbdd
Grant Li <grant@jostle.me>
2022-07-19 22:22:08 -0700

    adventure route codacy fix

--- backend/routes/Adventure.js
+++ backend/routes/Adventure.js
@@ -13,212 +13,166 @@ router.get("/", (req, res) => {
 // create new adventure
 router.post("/create", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.createAdventure(req.body);
+        await AdventureStore.createAdventure(req.body).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        } catch (err) { 
-            res.status(500).send(err);
-        }
+            
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // search adventures by filter
 router.post("/search-by-filter", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await FilterService.findAdventuresByFilter(req.body);
+        await FilterService.findAdventuresByFilter(req.body).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+            
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // discover adventure by user favorite categories
 router.get("/:userId/discover", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await FilterService.getRecommendationFeed(req.params.userId);
+        await FilterService.getRecommendationFeed(req.params.userId).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+            
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // search adventures by title
 router.get("/search-by-title", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.searchAdventuresByTitle(req.query.title);
+        await AdventureStore.searchAdventuresByTitle(req.query.title).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+            
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // get adventure details
 router.get("/:adventureId/detail", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.getAdventureDetail(req.params.adventureId);
+        await AdventureStore.getAdventureDetail(req.params.adventureId).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        } catch (err) { 
-            res.status(500).send(err);
-        }
+
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // update adventure details (you cannot manually update adventure status this way)
 router.put("/:adventureId/update", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.updateAdventure(req.params.adventureId, req.body);
+        await AdventureStore.updateAdventure(req.params.adventureId, req.body).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        } catch (err) {
-            res.status(500).send(err);
-        }
+
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // cancel the adventure and delete chat room
 router.put("/:adventureId/cancel", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.cancelAdventure(req.params.adventureId);
+        await AdventureStore.cancelAdventure(req.params.adventureId).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        } catch (err) {
-            res.status(500).send(err);
-        }
+
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // get ids of adventures owned or participated by user that are not cancelled
 router.get("/:userId/get-adventures", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.getUsersAdventures(req.params.userId);
+        await AdventureStore.getUsersAdventures(req.params.userId).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        } catch (err) {
-            res.status(500).send(err);
-        }
-    }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
     
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
+    }
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 // removes user from adventure, select new adventure admin/owner or delete adventure
 router.put("/:adventureId/quit", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await AdventureStore.removeAdventureParticipant(req.params.adventureId, req.query.userId);
-            if (result.code === 200) {
-                res.status(200).send(result.message);
-            }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+        await AdventureStore.removeAdventureParticipant(req.params.adventureId, req.query.userId).then(result => {
+            res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
-    
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 router.get("/nearby", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        try {
-            var result = await FilterService.getNearbyAdventures(req.query.city);
+        await FilterService.getNearbyAdventures(req.query.city).then(result => {
             if (result.code === 200) {
-                res.status(200).send(result.payload);
+                return res.status(200).send(result.payload);
             }
-            else {
-                res.status(result.code).send(result.message);
-            }
-        }
-        catch (err) {
-            res.status(500).send(err);
-        }
+
+            return res.status(result.code).send(result.message);
+        }, err => {
+            return res.status(500).send(err._message);
+        });
     }
-    else {
-        res.status(403).send({ message: Session.invalid_msg });
-    }
+
+    res.status(403).send({ message: Session.invalid_msg });
 });
 
 module.exports = router;
