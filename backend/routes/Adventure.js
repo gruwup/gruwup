97d081aefd3a4ee4424a7267bdef793e6cb2802e
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-07-20 19:20:00 -0700

    Stop adventure from sending multiple responses

--- backend/routes/Adventure.js
+++ backend/routes/Adventure.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 // create new adventure
 router.post("/create", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.createAdventure(req.body).then(result => {
+        return await AdventureStore.createAdventure(req.body).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -30,7 +30,7 @@ router.post("/create", async (req, res) => {
 // search adventures by filter
 router.post("/search-by-filter", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await FilterService.findAdventuresByFilter(req.body).then(result => {
+        return await FilterService.findAdventuresByFilter(req.body).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -47,7 +47,7 @@ router.post("/search-by-filter", async (req, res) => {
 // discover adventure by user favorite categories
 router.get("/:userId/discover", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await FilterService.getRecommendationFeed(req.params.userId).then(result => {
+        return await FilterService.getRecommendationFeed(req.params.userId).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -64,7 +64,7 @@ router.get("/:userId/discover", async (req, res) => {
 // search adventures by title
 router.get("/search-by-title", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.searchAdventuresByTitle(req.query.title).then(result => {
+        return await AdventureStore.searchAdventuresByTitle(req.query.title).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -81,7 +81,7 @@ router.get("/search-by-title", async (req, res) => {
 // get adventure details
 router.get("/:adventureId/detail", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.getAdventureDetail(req.params.adventureId).then(result => {
+        return await AdventureStore.getAdventureDetail(req.params.adventureId).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -98,7 +98,7 @@ router.get("/:adventureId/detail", async (req, res) => {
 // update adventure details (you cannot manually update adventure status this way)
 router.put("/:adventureId/update", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.updateAdventure(req.params.adventureId, req.body).then(result => {
+        return await AdventureStore.updateAdventure(req.params.adventureId, req.body).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -115,7 +115,7 @@ router.put("/:adventureId/update", async (req, res) => {
 // cancel the adventure and delete chat room
 router.put("/:adventureId/cancel", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.cancelAdventure(req.params.adventureId).then(result => {
+        return await AdventureStore.cancelAdventure(req.params.adventureId).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -132,7 +132,7 @@ router.put("/:adventureId/cancel", async (req, res) => {
 // get ids of adventures owned or participated by user that are not cancelled
 router.get("/:userId/get-adventures", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.getUsersAdventures(req.params.userId).then(result => {
+        return await AdventureStore.getUsersAdventures(req.params.userId).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
@@ -149,7 +149,7 @@ router.get("/:userId/get-adventures", async (req, res) => {
 // removes user from adventure, select new adventure admin/owner or delete adventure
 router.put("/:adventureId/quit", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await AdventureStore.removeAdventureParticipant(req.params.adventureId, req.query.userId).then(result => {
+        return await AdventureStore.removeAdventureParticipant(req.params.adventureId, req.query.userId).then(result => {
             res.status(result.code).send(result.message);
         }, err => {
             return res.status(500).send(err._message);
@@ -161,7 +161,7 @@ router.put("/:adventureId/quit", async (req, res) => {
 
 router.get("/nearby", async (req, res) => {
     if (Session.validSession(req.headers.cookie) || TestMode.on) {
-        await FilterService.getNearbyAdventures(req.query.city).then(result => {
+        return await FilterService.getNearbyAdventures(req.query.city).then(result => {
             if (result.code === 200) {
                 return res.status(200).send(result.payload);
             }
