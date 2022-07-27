37af45a2852e94f62428d13730eaa318028dc78a
Sijan <sijuichi1@gmail.com>
2022-07-26 19:45:58 -0700

    Configure uiautomator, test to launch app

--- frontend/app/src/androidTest/java/com/cpen321/gruwup/ProfileTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/gruwup/ProfileTest.java
@@ -6,9 +6,25 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiScrollable;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,9 +38,32 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ProfileTest {
+
+
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "com.example.android.testing.uiautomator.BasicSample";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice device;
+
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void testLogin() throws UiObjectNotFoundException {
+
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        device.pressHome();
+
+// Bring up the default launcher by searching for a UI component
+// that matches the content description for the launcher button.
+        UiObject allAppsButton = device
+                .findObject(new UiSelector().description("gruwup"));
+
+// Perform a click on the button to load the launcher.
+        allAppsButton.clickAndWaitForNewWindow();
+
+    }
 
     @Test
     public void manageProfile() {
