80e0e6b1424e7d299f67c7795264f2a4870b07d2
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-07-30 17:12:36 -0700

    Add back UserStore mock

---
+++ backend/store/__mocks__/UserStore.js
@@ -0,0 +1,27 @@
+const MockTestData = require('../../test/mocks/MockTestData');
+
+module.exports = class RequestStoreMocks {
+    static getUserProfile = jest.fn((userId) => {
+        var result =  {
+            code: 200,
+            message: "User Profile found",
+            payload: MockTestData.testProfile1
+        }
+        if (userId === "1") {
+            result = {
+                code: 404,
+                message: "User Profile not found"
+            }
+        }
+        else if (userId === "2") {
+            result = {
+                code: 200,
+                message: "User Profile found",
+                payload: MockTestData.testProfile2
+            }
+        }
+        return new Promise ((resolve, reject) => {
+            resolve(result);
+        });
+    });
+} 
