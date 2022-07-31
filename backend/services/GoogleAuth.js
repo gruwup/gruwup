b1d8e52f2334e1f938881d5ba483d9f2de12eccb
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-07-30 17:02:25 -0700

    Update backend test script and some formatting changes

--- backend/services/GoogleAuth.js
+++ backend/services/GoogleAuth.js
@@ -1,10 +1,8 @@
 const {OAuth2Client} = require('google-auth-library');
 
-CLIENT_ID="689956521180-el1vflpf7lukac95p4u4s57qv84oq46l.apps.googleusercontent.com"
-
 //https://developers.google.com/identity/sign-in/web/backend-auth
 const GoogleAuth = {
-    validateToken: (token, client_id = CLIENT_ID) => {
+    validateToken: (token, client_id) => {
         const client = new OAuth2Client(client_id);
         async function verify() {
             const ticket = await client.verifyIdToken({
