7946feda1c86fad91ab287ed04f47507b5f8b686
Kaleb Hui <kaleb.e.hui@gmail.com>
2022-08-02 18:58:28 -0700

    Fix codacy style issue wit AdventureStore mock

--- backend/store/__mocks__/AdventureStore.js
+++ backend/store/__mocks__/AdventureStore.js
@@ -74,12 +74,13 @@ module.exports = class AdventureStoreMocks {
     static findAdventuresByFilter = jest.fn((filter) => {
         var data = [MockTestData.testAdventure1, MockTestData.testAdventure2]
         var result = [];
+        var category;
         for (var i = 0; i < data.length; i++) {
             if (filter.city === data[i].city) {
                 if (result.indexOf(data[i]) === -1) result.push(data[i]);
             }
             if (filter.$and && filter.$and[2].category) { //test getRecommendation
-                var category = filter.$and[2].category;
+                category = filter.$and[2].category;
                 if (category.$in) {
                     for (var j = 0; j < category.$in.length; j++) {
                         if (category.$in[j] === data[i].category) {
@@ -89,7 +90,7 @@ module.exports = class AdventureStoreMocks {
                 }
             }
             else if (filter.$and && filter.$and[0].category.$in && filter.$and[1].dateTime.$lte) { //findByFilter
-                var category = filter.$and[0].category.$in;
+                category = filter.$and[0].category.$in;
                 var dateTime = filter.$and[1].dateTime.$lte;
                 for (var k = 0; k < category.length; k++) {
                     if (category[k] === data[i].category && dateTime >= data[i].dateTime) {
