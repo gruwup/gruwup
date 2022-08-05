4a0ddea0a0864747aabe8d5962abf05d6ccc9cd8
Sijan <sijuichi1@gmail.com>
2022-08-05 01:04:31 -0700

    Refactored Espresso Tests to start from Login Screen

--- frontend/app/src/androidTest/java/com/cpen321/gruwup/CreateAdventureTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/gruwup/CreateAdventureTest.java
@@ -15,6 +15,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
@@ -28,7 +29,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.annotation.CheckResult;
+import androidx.test.espresso.AmbiguousViewMatcherException;
 import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.NoMatchingRootException;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -52,8 +59,37 @@ import org.junit.runner.RunWith;
 public class CreateAdventureTest {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<LogInActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(LogInActivity.class);
+
+
+    @CheckResult
+    public static boolean exists(ViewInteraction interaction) {
+        try {
+            interaction.perform(new ViewAction() {
+                @Override public Matcher<View> getConstraints() {
+                    return any(View.class);
+                }
+                @Override public String getDescription() {
+                    return "check for existence";
+                }
+                @Override
+                public void perform(UiController uiController, View view) {
+                    // no op, if this is run, then the execution will continue after .perform(...)
+                }
+            });
+            return true;
+        } catch (AmbiguousViewMatcherException ex) {
+            // if there's any interaction later with the same matcher, that'll fail anyway
+            return true; // we found more than one
+        } catch (NoMatchingViewException ex) {
+            return false;
+        } catch (NoMatchingRootException ex) {
+            // optional depending on what you think "exists" means
+            return false;
+        }
+    }
+
 
     @Rule
     public GrantPermissionRule mGrantPermissionRule =
@@ -91,6 +127,21 @@ public class CreateAdventureTest {
 
     @Test
     public void createAdventureTestSuccess() {
+
+        ViewInteraction hj = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+        if (exists(hj)){
+            hj.perform(click());
+        }
+
         ViewInteraction materialTextView = onView(
                 allOf(withId(R.id.create_adventure), withText(" +"),
                         childAtPosition(
@@ -204,6 +255,21 @@ public class CreateAdventureTest {
 
     @Test
     public void createAdventureTestFailNoImage() {
+
+        ViewInteraction hj = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+        if (exists(hj)){
+            hj.perform(click());
+        }
+
         ViewInteraction materialTextView = onView(
                 allOf(withId(R.id.create_adventure), withText(" +"),
                         childAtPosition(
@@ -306,6 +372,21 @@ public class CreateAdventureTest {
 
     @Test
     public void createAdventureTestFailNoTime() {
+
+        ViewInteraction hj = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+        if (exists(hj)){
+            hj.perform(click());
+        }
+
         ViewInteraction materialTextView = onView(
                 allOf(withId(R.id.create_adventure), withText(" +"),
                         childAtPosition(
@@ -372,6 +453,21 @@ public class CreateAdventureTest {
 
     @Test
     public void createAdventureTestCancelled() {
+
+        ViewInteraction hj = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+        if (exists(hj)){
+            hj.perform(click());
+        }
+        
         ViewInteraction materialTextView = onView(
                 allOf(withId(R.id.create_adventure), withText(" +"),
                         childAtPosition(
--- frontend/app/src/androidTest/java/com/cpen321/gruwup/ProfileTest.java
+++ frontend/app/src/androidTest/java/com/cpen321/gruwup/ProfileTest.java
@@ -5,18 +5,26 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.is;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import androidx.annotation.CheckResult;
+import androidx.test.espresso.AmbiguousViewMatcherException;
+import androidx.test.espresso.NoMatchingRootException;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -35,8 +43,8 @@ import org.junit.runner.RunWith;
 public class ProfileTest {
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<LogInActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(LogInActivity.class);
 
     @Rule
     public GrantPermissionRule mGrantPermissionRule =
@@ -46,8 +54,49 @@ public class ProfileTest {
                     "android.permission.INTERNET",
                     "android.permission.ACCESS_WIFI_STATE");
 
+    @CheckResult
+    public static boolean exists(ViewInteraction interaction) {
+        try {
+            interaction.perform(new ViewAction() {
+                @Override public Matcher<View> getConstraints() {
+                    return any(View.class);
+                }
+                @Override public String getDescription() {
+                    return "check for existence";
+                }
+                @Override
+                public void perform(UiController uiController, View view) {
+                    // no op, if this is run, then the execution will continue after .perform(...)
+                }
+            });
+            return true;
+        } catch (AmbiguousViewMatcherException ex) {
+            // if there's any interaction later with the same matcher, that'll fail anyway
+            return true; // we found more than one
+        } catch (NoMatchingViewException ex) {
+            return false;
+        } catch (NoMatchingRootException ex) {
+            // optional depending on what you think "exists" means
+            return false;
+        }
+    }
+
     @Test
     public void profTest() {
+
+        ViewInteraction hj = onView(
+                allOf(withText("Sign in"),
+                        childAtPosition(
+                                allOf(withId(R.id.sign_in_button),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                0),
+                        isDisplayed()));
+
+        if (exists(hj)){
+            hj.perform(click());
+        }
 
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.nav_profile), withContentDescription("Profile"),
